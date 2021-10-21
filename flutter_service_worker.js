ef3ddb6afcb04989d78bdcbe8e4ff72fea9c4a5b
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-21 10:15:20 +0100

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "fb800d37cb51ce25f91a8114a375508b",
-"index.html": "ff5280f16468138bd7d9aefc2609c975",
-"/": "ff5280f16468138bd7d9aefc2609c975",
-"main.dart.js": "71436c54e1aec777173f638a6b939aec",
+"index.html": "292e00db5d77851efc16f8ba258769c4",
+"/": "292e00db5d77851efc16f8ba258769c4",
+"main.dart.js": "af317d6d495a59eb77d9dcea06ab4c26",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
